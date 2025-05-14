3ed95e76689163ea912f0f4f9927239e992cfbbe
Brenno <brenno.oliveira@souunit.com.br>
2025-05-14 11:15:18 -0300

    fix: Vite config with mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // base: '/money-track-frontend/',
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react()],
+    base: isProduction ? '/money-track-frontend/' : '/',
+    build: {
+      outDir: 'dist',
+      sourcemap: isProduction,
+    },
+  };
 })
